d82ca97250228e367a8d102831fdf5cded75a35d
Uladzimir Aleshka <uladzimir.aleshka@gmail.com>
2019-07-09 10:34:22 +0300

    Github OAuth

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -21,6 +21,50 @@
 
 <script>
 import { openURL } from 'quasar';
+import querystring from 'querystring';
+import axios from 'axios';
+
+const GITHUB_CLIENT_ID = '8b20e17f2c8c05c29536';
+const GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
+const GET_TOKEN_URL = 'http://localhost:3000/api/token';
+const REDIRECT_URL = window.location.origin;
+const SCOPE = 'repo';
+const LS_KEY = 'token';
+
+function navigateToGithubAuth() {
+  const query = {
+    client_id: GITHUB_CLIENT_ID,
+    redirect_uri: REDIRECT_URL,
+    scope: SCOPE,
+  };
+  const queryStr = querystring.stringify(query);
+
+  window.location.href = `${GITHUB_AUTH_URL}?${queryStr}`;
+}
+
+if (window.location.search.indexOf('code=') > -1) {
+  const { code, ...rest } = querystring.parse(window.location.search.slice(1));
+
+  axios({
+    method: 'post',
+    url: GET_TOKEN_URL,
+    data: {
+      code,
+    },
+  })
+    .then((response) => {
+      localStorage.setItem(LS_KEY, JSON.stringify(response.data));
+      window.location.search = querystring.stringify(rest);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+} else {
+  const token = localStorage.getItem(LS_KEY);
+  if (!token) {
+    navigateToGithubAuth();
+  }
+}
 
 export default {
   name: 'MyLayout',
